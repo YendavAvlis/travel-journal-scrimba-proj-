07d6c2244e26999e0044815bf47f5a80a1cdff23
YendavAvlis <vadneysilva@hotmail.com>
2025-06-25 18:40:16 +0200

    Header Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { FaGlobeAfrica } from "react-icons/fa";
+
 export default function Header() {
     return (
-        <header className="w-full flex items-center justify-center bg-blue-400">
-            <h1 className="text-5xl">Header</h1>
+        <header className="w-full h-[80px] flex gap-4 items-center justify-center bg-orange-300">
+            <FaGlobeAfrica className="text-3xl"/>
+            <h1 className="text-2xl uppercase tracking-wide font-bold">Travel Journal</h1>
         </header>
     )
 }
