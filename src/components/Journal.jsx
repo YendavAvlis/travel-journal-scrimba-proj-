37677bd0f61b1497d0aef2c1fcbe2d4aad89fcfa
YendavAvlis <vadneysilva@hotmail.com>
2025-06-25 19:28:26 +0200

    Journal Added

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,7 +1,37 @@
+import data from "../../data"
+import Marker from "../assets/marker.png"
+
 export default function Journal() {
     return (
         <main>
-            Journal
+
+                {
+                    data.map((entry) => (
+                        <div key={entry.id} className="py-4 md:flex items-center justify-center bg-amber-50">
+                            <div className="overflow-hidden max-w-[400px] max-h-[200px] rounded-2xl m-8">
+                                <img
+                                    src={entry.img.src}
+                                    alt={entry.img.alt}
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                            <div className="px-8">
+                                <div className="flex gap-2">
+                                    <img src={Marker} alt="Marker" width={16} className=""/>
+                                    <span className="mx-1 uppercase tracking-wide font-medium">{entry.country}</span>
+                                    <a href={entry.googleMapsLink} className="text-slate-500 underline text-md">View on Google Maps</a>
+                                </div>
+                                <div>
+                                    <h1 className="text-5xl font-bold pt-2 pb-12">{entry.title}</h1>
+                                    <span className="font-bold block mb-4">{entry.dates}</span>
+                                    <p className="">{entry.text}</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))
+                }
+
         </main>
     )
 }
